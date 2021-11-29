dbc010c680e5709794e083ad6fbf160ee9a73234
LeeJuOh <>
2021-11-29 23:56:01 +0900

    fix: 쓰레기 주석 삭제

--- src/main/java/com/pet/Application.java
+++ src/main/java/com/pet/Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-    // test
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
