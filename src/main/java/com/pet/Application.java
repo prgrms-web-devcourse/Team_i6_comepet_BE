8bc208d01023c7da16a93657086d0bff53255717
yhh1056 <58363663+yhh1056@users.noreply.github.com>
2021-12-02 16:50:18 +0900

    [CP-40] S3 Code Deploy 문서 수정 (#41)
    * fix: s3 Code Deploy 트리거 수정
    
    해당 경로가 맞지 않아 수정
    
    * fix: 출력문 제거

--- src/main/java/com/pet/Application.java
+++ src/main/java/com/pet/Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("s3 upload test");
         SpringApplication.run(Application.class, args);
     }
 
