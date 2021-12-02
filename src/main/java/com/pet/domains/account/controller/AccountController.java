e1f16eb7366dca36bd894dac1a8075564e467219
venzym <venzym@naver.com>
2021-12-02 18:49:16 +0900

    feat: 서버 확인용 health check api 추가 (#49)

--- src/main/java/com/pet/domains/account/controller/AccountController.java
+++ src/main/java/com/pet/domains/account/controller/AccountController.java
@@ -1,5 +1,16 @@
 package com.pet.domains.account.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
 public class AccountController {
+
+    @GetMapping
+    public String healthCheck() {
+        return "OK";
+    }
 
 }
