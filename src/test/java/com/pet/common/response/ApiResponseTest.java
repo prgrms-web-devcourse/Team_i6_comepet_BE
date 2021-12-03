6585e2743e64e5d1fed6995a179bbd27fab77e63
yhh1056 <58363663+yhh1056@users.noreply.github.com>
2021-12-03 14:10:10 +0900

    feat: test case 추가

--- src/test/java/com/pet/common/response/ApiResponseTest.java
+++ src/test/java/com/pet/common/response/ApiResponseTest.java
@@ -10,7 +10,7 @@ class ApiResponseTest {
 
     @Test
     @DisplayName("ApiResponse 생성 테스트")
-    void newApiResponse() {
+    void newApiResponseTest() {
         ApiResponse<String> testResponse = ApiResponse.ok("Test");
 
         assertThat(testResponse.getData()).isEqualTo("Test");
@@ -19,9 +19,17 @@ class ApiResponseTest {
 
     @Test
     @DisplayName("ApiResponse null 주입 테스트")
-    void apiResponseIsFinalObject() {
+    void apiResponseIsNullTest() {
         assertThatNullPointerException()
             .isThrownBy(() -> new ApiResponse(null))
+            .withMessage("data must be not null");
+    }
+
+    @Test
+    @DisplayName("ApiResponse empty 주입 테스트")
+    void apiResponseIsEmptyDataTest() {
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> new ApiResponse(Collections.emptyList()))
             .withMessage("data must be not null");
     }
 
