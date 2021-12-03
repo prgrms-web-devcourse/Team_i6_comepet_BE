9ef4dd5458ea3b70b858ee77fdd8ccc7b595170e
yhh1056 <yhh1056@naver.com>
2021-12-03 17:40:19 +0900

    test: test 포멧 변경

--- src/test/java/com/pet/domains/area/controller/CityControllerTest.java
+++ src/test/java/com/pet/domains/area/controller/CityControllerTest.java
@@ -1,19 +1,16 @@
 package com.pet.domains.area.controller;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
-@ExtendWith(SpringExtension.class)
 @WebMvcTest(CityController.class)
 @DisplayName("시도/시군구 컨트롤러 테스트")
 class CityControllerTest {
@@ -23,11 +20,16 @@ class CityControllerTest {
 
     @Test
     @DisplayName("시도/시군구 조회 성공 테스트")
-    void getCities() throws Exception {
-        mockMvc.perform(get("/api/v1/cities"))
+    void getCitiesTest() throws Exception {
+        // given
+        // when
+        ResultActions resultActions = mockMvc.perform(get("/api/v1/cities"));
+        // then
+        resultActions
             .andDo(print())
             .andExpectAll(
                 status().isOk(),
+                content().contentType(MediaType.APPLICATION_JSON),
                 jsonPath("data").exists(),
                 jsonPath("$.data.cities").isArray(),
                 jsonPath("$.data.cities[0].id").isNumber(),
@@ -35,9 +37,8 @@ class CityControllerTest {
                 jsonPath("$.data.cities[0].towns").isArray(),
                 jsonPath("$.data.cities[0].towns[0].id").isNumber(),
                 jsonPath("$.data.cities[0].towns[0].name").isString(),
-                jsonPath("$.serverDateTime").isString(),
-                content().contentType(MediaType.APPLICATION_JSON)
-            )
-        ;
+                jsonPath("$.serverDateTime").isString()
+            );
     }
+
 }
