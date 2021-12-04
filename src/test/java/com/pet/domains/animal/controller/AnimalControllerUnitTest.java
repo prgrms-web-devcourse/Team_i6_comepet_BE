9fbbebaf8dab1e5428c1f2315e7a43807a4c7ce1
LeeJuOh <>
2021-12-04 15:07:32 +0900

    feat: rest docs 추가

--- src/test/java/com/pet/domains/animal/controller/AnimalControllerUnitTest.java
+++ src/test/java/com/pet/domains/animal/controller/AnimalControllerUnitTest.java
@@ -1,19 +1,31 @@
 package com.pet.domains.animal.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.JsonFieldType.ARRAY;
+import static org.springframework.restdocs.payload.JsonFieldType.NUMBER;
+import static org.springframework.restdocs.payload.JsonFieldType.OBJECT;
+import static org.springframework.restdocs.payload.JsonFieldType.STRING;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 @WebMvcTest(AnimalController.class)
+@AutoConfigureRestDocs
 @DisplayName("동물/품종 컨트롤러 테스트")
 class AnimalControllerUnitTest {
 
@@ -25,21 +37,26 @@ class AnimalControllerUnitTest {
     void getAnimalsTest() throws Exception {
         // given
         // when
-        ResultActions resultActions = mockMvc.perform(get("/api/v1/animals"))
-            .andDo(print());
+        ResultActions resultActions = mockMvc.perform(get("/api/v1/animals")
+            .accept(MediaType.APPLICATION_JSON));
 
         // then
-        resultActions.andExpectAll(
-            status().isOk(),
-            jsonPath("data").exists(),
-            jsonPath("$.data.animals").isArray(),
-            jsonPath("$.data.animals[0].id").isNumber(),
-            jsonPath("$.data.animals[0].name").isString(),
-            jsonPath("$.data.animals[0].kinds").isArray(),
-            jsonPath("$.data.animals[0].kinds[0].id").isNumber(),
-            jsonPath("$.data.animals[0].kinds[0].name").isString(),
-            jsonPath("$.serverDateTime").isString(),
-            content().contentType(MediaType.APPLICATION_JSON)
-        );
+        resultActions.andExpect(status().isOk())
+            .andDo(document("get-animals",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                responseHeaders(
+                    headerWithName(HttpHeaders.CONTENT_TYPE).description(MediaType.APPLICATION_JSON_VALUE)
+                ),
+                responseFields(
+                    fieldWithPath("data").type(OBJECT).description("응답 데이터"),
+                    fieldWithPath("data.animals").type(ARRAY).description("동물"),
+                    fieldWithPath("data.animals[0].id").type(NUMBER).description("동물 id"),
+                    fieldWithPath("data.animals[0].name").type(STRING).description("동물 이름"),
+                    fieldWithPath("data.animals[0].kinds").type(ARRAY).description("품종 id"),
+                    fieldWithPath("data.animals[0].kinds[0].id").type(NUMBER).description("품종 id"),
+                    fieldWithPath("data.animals[0].kinds[0].name").type(STRING).description("품종 id"),
+                    fieldWithPath("serverDateTime").type(STRING).description("서버 응답 시간")))
+            );
     }
 }
