b70c8a1d6cd782b904743663bcb2c31720437b35
LeeJuOh <>
2021-12-05 04:34:06 +0900

    feat: 게시글 통계 추가

---
+++ src/main/java/com/pet/domains/statistics/controller/PostStatisticsController.java
@@ -0,0 +1,20 @@
+package com.pet.domains.statistics.controller;
+
+import com.pet.common.response.ApiResponse;
+import com.pet.domains.statistics.dto.response.PostStatisticsReadResult;
+import java.time.LocalDateTime;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/statistics")
+@RestController
+public class PostStatisticsController {
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ApiResponse<PostStatisticsReadResult> getPostStatistics() {
+        return ApiResponse.ok(PostStatisticsReadResult.of(124L, 53L, 27L, 564L, LocalDateTime.now()));
+    }
+
+}
---
+++ src/test/java/com/pet/domains/statistics/controller/PostStatisticsControllerTest.java
@@ -0,0 +1,60 @@
+package com.pet.domains.statistics.controller;
+
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.JsonFieldType.NUMBER;
+import static org.springframework.restdocs.payload.JsonFieldType.OBJECT;
+import static org.springframework.restdocs.payload.JsonFieldType.STRING;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+@WebMvcTest(value = PostStatisticsController.class)
+@AutoConfigureRestDocs
+@DisplayName("통계 컨트롤러 docs 테스트")
+class PostStatisticsControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("게시글 통계 데이터 조회 테스트")
+    void getPostStatisticsTest() throws Exception {
+        // given
+        // when
+        ResultActions resultActions = mockMvc.perform(get("/api/v1/statistics")
+            .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        resultActions.andExpect(status().isOk())
+            .andDo(document("get-post-statistics}",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                responseHeaders(
+                    headerWithName(HttpHeaders.CONTENT_TYPE).description(MediaType.APPLICATION_JSON_VALUE)
+                ),
+                responseFields(
+                    fieldWithPath("data").type(OBJECT).description("응답 데이터"),
+                    fieldWithPath("data.missing").type(NUMBER).description("실종 게시글 수"),
+                    fieldWithPath("data.detection").type(NUMBER).description("목격 게시글 수"),
+                    fieldWithPath("data.protection").type(NUMBER).description("보호 게시글 수"),
+                    fieldWithPath("data.completion").type(NUMBER).description("완료 게시글 수"),
+                    fieldWithPath("data.date").type(STRING).description("통계 시간"),
+                    fieldWithPath("serverDateTime").type(STRING).description("서버 응답 시간")))
+            );
+    }
+}
