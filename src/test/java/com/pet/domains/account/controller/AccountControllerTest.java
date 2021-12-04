438d1e5b9bf932cfd37e81b706d6fe34ce88d94a
yhh1056 <yhh1056@naver.com>
2021-12-04 19:25:54 +0900

    feat: 회원 북마크 게시물 조회

--- src/test/java/com/pet/domains/account/controller/AccountControllerTest.java
+++ src/test/java/com/pet/domains/account/controller/AccountControllerTest.java
@@ -5,6 +5,7 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.JsonFieldType.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -325,6 +326,98 @@ class AccountControllerTest {
                     fieldWithPath("data.posts[0].postTags[0].id").type(NUMBER).description("게시물 태그 id"),
                     fieldWithPath("data.posts[0].postTags[0].name").type(STRING).description("게시물 태그 이름"),
                     fieldWithPath("data.posts[0].thumbnail").type(STRING).description("게시물 썸네일"),
+                    fieldWithPath("data.totalElements").type(NUMBER).description("총 게시물 수"),
+                    fieldWithPath("data.last").type(BOOLEAN).description("마지막 페이지 여부"),
+                    fieldWithPath("data.size").type(NUMBER).description("페이지 크기"),
+                    fieldWithPath("serverDateTime").type(STRING).description("서버 응답 시간")
+                ))
+            );
+
+    }
+
+    @Test
+    @DisplayName("회원 북마크(실종/보호) 조회")
+    void getAccountMissingBookmarkPostTest() throws Exception {
+        // given
+        // when
+        ResultActions resultActions = mockMvc.perform(get("/api/v1/me/bookmarks?status=missing")
+            .header(HttpHeaders.AUTHORIZATION, JwtMockToken.MOCK_TOKEN)
+            .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        resultActions
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("get-account-missing-bookmark-posts",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestHeaders(
+                    headerWithName(HttpHeaders.CONTENT_TYPE).description(MediaType.APPLICATION_JSON_VALUE),
+                    headerWithName(HttpHeaders.AUTHORIZATION).description("jwt token")
+                ),
+                responseHeaders(
+                    headerWithName(HttpHeaders.CONTENT_TYPE).description(MediaType.APPLICATION_JSON_VALUE)
+                ),
+                requestParameters(
+                    parameterWithName("status").description("게시물 종류")
+                ),
+                responseFields(
+                    fieldWithPath("data").type(OBJECT).description("응답 데이터"),
+                    fieldWithPath("data.posts").type(ARRAY).description("게시물"),
+                    fieldWithPath("data.posts[0].id").type(NUMBER).description("게시물 id"),
+                    fieldWithPath("data.posts[0].animalKind").type(STRING).description("품종"),
+                    fieldWithPath("data.posts[0].sexType").type(STRING).description("성별"),
+                    fieldWithPath("data.posts[0].place").type(STRING).description("위치"),
+                    fieldWithPath("data.posts[0].createdAt").type(STRING).description("게시물 생성일"),
+                    fieldWithPath("data.posts[0].sexType").type(STRING).description("성별"),
+                    fieldWithPath("data.posts[0].thumbnail").type(STRING).description("게시물 썸네일"),
+                    fieldWithPath("data.posts[0].bookmarkCount").type(NUMBER).description("북마크 수"),
+                    fieldWithPath("data.totalElements").type(NUMBER).description("총 게시물 수"),
+                    fieldWithPath("data.last").type(BOOLEAN).description("마지막 페이지 여부"),
+                    fieldWithPath("data.size").type(NUMBER).description("페이지 크기"),
+                    fieldWithPath("serverDateTime").type(STRING).description("서버 응답 시간")
+                ))
+            );
+
+    }
+
+    @Test
+    @DisplayName("회원 북마크(보호소) 조회")
+    void getAccountShelterBookmarkPostTest() throws Exception {
+        // given
+        // when
+        ResultActions resultActions = mockMvc.perform(get("/api/v1/me/bookmarks?status=shelter")
+            .header(HttpHeaders.AUTHORIZATION, JwtMockToken.MOCK_TOKEN)
+            .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        resultActions
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("get-account-shelter-bookmark-posts",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestHeaders(
+                    headerWithName(HttpHeaders.CONTENT_TYPE).description(MediaType.APPLICATION_JSON_VALUE),
+                    headerWithName(HttpHeaders.AUTHORIZATION).description("jwt token")
+                ),
+                responseHeaders(
+                    headerWithName(HttpHeaders.CONTENT_TYPE).description(MediaType.APPLICATION_JSON_VALUE)
+                ),
+                requestParameters(
+                    parameterWithName("status").description("게시물 종류")
+                ),
+                responseFields(
+                    fieldWithPath("data").type(OBJECT).description("응답 데이터"),
+                    fieldWithPath("data.posts").type(ARRAY).description("게시물"),
+                    fieldWithPath("data.posts[0].id").type(NUMBER).description("게시물 id"),
+                    fieldWithPath("data.posts[0].animalKind").type(STRING).description("품종"),
+                    fieldWithPath("data.posts[0].sexType").type(STRING).description("성별"),
+                    fieldWithPath("data.posts[0].place").type(STRING).description("위치"),
+                    fieldWithPath("data.posts[0].createdAt").type(STRING).description("게시물 생성일"),
+                    fieldWithPath("data.posts[0].sexType").type(STRING).description("성별"),
+                    fieldWithPath("data.posts[0].thumbnail").type(STRING).description("게시물 썸네일"),
+                    fieldWithPath("data.posts[0].bookmarkCount").type(NUMBER).description("북마크 수"),
                     fieldWithPath("data.totalElements").type(NUMBER).description("총 게시물 수"),
                     fieldWithPath("data.last").type(BOOLEAN).description("마지막 페이지 여부"),
                     fieldWithPath("data.size").type(NUMBER).description("페이지 크기"),
