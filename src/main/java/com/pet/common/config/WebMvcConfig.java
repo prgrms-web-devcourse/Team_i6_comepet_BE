8ace51390f2fd53f3dfaa980f32cbd9ae399470d
LeeJuOh <>
2021-12-06 17:34:12 +0900

    fix: cors 풀기

--- src/main/java/com/pet/common/config/WebMvcConfig.java
+++ src/main/java/com/pet/common/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.pet.common.config;
 
 import com.pet.common.interceptor.JwtAuthenticationInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,4 +16,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
             .addPathPatterns("/api/v1/**");
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**").allowedOrigins("*");
+    }
 }
