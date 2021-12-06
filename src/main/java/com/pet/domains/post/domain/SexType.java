7d2d53c069c01a5c7219590a905395b691cfca89
venzym <venzym@naver.com>
2021-12-06 15:15:19 +0900

    style: enum @JsonCreator 제거

--- src/main/java/com/pet/domains/post/domain/SexType.java
+++ src/main/java/com/pet/domains/post/domain/SexType.java
@@ -1,19 +1,7 @@
 package com.pet.domains.post.domain;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-
 public enum SexType {
     MALE,
     FEMALE,
     UNKNOWN;
-
-    @JsonCreator
-    public static SexType validateSexType(String sex) {
-        for (SexType sexType : SexType.values()) {
-            if (sexType.name().equals(sex)) {
-                return sexType;
-            }
-        }
-        throw new IllegalArgumentException("It is not a value corresponding to the animal gender of the post.");
-    }
 }
