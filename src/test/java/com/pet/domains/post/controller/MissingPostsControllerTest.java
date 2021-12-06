72f394c35878912b3901ac7697367a24f62d46f6
LeeJuOh <56071126+LeeJuOh@users.noreply.github.com>
2021-12-06 15:11:26 +0900

    [CP-72] feat: 동물, 보호소 게시글, 댓글 mocking 데이터 추가 (#84)
    * feat: 동물, 품종 조회 api mocking
    
    * chore: given, when, then convention
    
    * chore: 경로 수정
    
    * chore: 경로 수정
    
    * feat: 응답헤더 설정
    
    * feat: rest docs 추가
    
    * chore: naming 수정
    
    * chore: dto & controller 추가
    
    * feat: 테스트 및 jackson 수정
    
    * feat: 단건 조회 추가
    
    * feat: docs 설명 수정
    
    * feat: 북마크 mock api 추가
    
    * feat: 특정 게시글의 댓글 api mock 추가
    
    * chore: lint 해결
    
    * feat: gradle rest docs html 세팅
    
    * feat: comment create request field 추가
    
    * feat: comment update request field 추가
    
    * feat: 게시글 통계 추가
    
    * feat: jwt 토큰 헤더 추가
    
    * feat: getter로 사용 및 print() 추가
    
    * feat: docs jwt header 추가
    
    * feat: pr comment 반영
    
    * feat: 빈 응답 바디
    
    * feat: map return
    
    * style: value key 추가
    
    * fix: import 추가
    
    Co-authored-by: LeeJuOh <>

--- src/test/java/com/pet/domains/post/controller/MissingPostsControllerTest.java
+++ src/test/java/com/pet/domains/post/controller/MissingPostsControllerTest.java
@@ -1,21 +1,25 @@
-package com.pet.domains.area.controller;
+package com.pet.domains.post.controller;
 
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.JsonFieldType.ARRAY;
+import static org.springframework.restdocs.payload.JsonFieldType.BOOLEAN;
 import static org.springframework.restdocs.payload.JsonFieldType.NUMBER;
 import static org.springframework.restdocs.payload.JsonFieldType.OBJECT;
 import static org.springframework.restdocs.payload.JsonFieldType.STRING;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.pet.common.jwt.JwtMockToken;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,30 +30,35 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-@WebMvcTest(value = CityController.class)
+@WebMvcTest(value = MissingPostsController.class)
 @AutoConfigureRestDocs
-@DisplayName("시도/시군구 컨트롤러 테스트")
-class CityControllerTest {
+@DisplayName("실종 게시글 컨트롤러 docs 테스트")
+class MissingPostsControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    @DisplayName("시도/시군구 조회 성공 테스트")
-    void getCitiesTest() throws Exception {
+    @DisplayName("특정 실종게시글의 댓글 리스트 조회 테스트")
+    void getShelterPostsTest() throws Exception {
         // given
         // when
-        ResultActions resultActions = mockMvc.perform(get("/api/v1/cities")
-            .accept(MediaType.APPLICATION_JSON));
+        ResultActions resultActions = mockMvc.perform(get("/api/v1/missing-posts/{postId}/comments", 1L)
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .header(HttpHeaders.AUTHORIZATION, JwtMockToken.MOCK_TOKEN));
 
         // then
         resultActions
             .andDo(print())
             .andExpect(status().isOk())
-            .andDo(document("get-cities",
+            .andDo(document("get-missing-posts-comments",
                 preprocessRequest(prettyPrint()),
                 preprocessResponse(prettyPrint()),
+                pathParameters(
+                    parameterWithName("postId").description("실종 게시글 아이디")
+                ),
                 requestHeaders(
+                    headerWithName(HttpHeaders.AUTHORIZATION).description("jwt token"),
                     headerWithName(HttpHeaders.ACCEPT).description(MediaType.APPLICATION_JSON_VALUE)
                 ),
                 responseHeaders(
@@ -57,14 +66,18 @@ class CityControllerTest {
                 ),
                 responseFields(
                     fieldWithPath("data").type(OBJECT).description("응답 데이터"),
-                    fieldWithPath("data.cities").type(ARRAY).description("시도"),
-                    fieldWithPath("data.cities[0].id").type(NUMBER).description("시도 id"),
-                    fieldWithPath("data.cities[0].name").type(STRING).description("시도 이름"),
-                    fieldWithPath("data.cities[0].towns").type(ARRAY).description("시도 id"),
-                    fieldWithPath("data.cities[0].towns[0].id").type(NUMBER).description("시도 id"),
-                    fieldWithPath("data.cities[0].towns[0].name").type(STRING).description("시도 id"),
+                    fieldWithPath("data.comments").type(ARRAY).description("댓글 목록"),
+                    fieldWithPath("data.comments[].id").type(NUMBER).description("댓글 아이디"),
+                    fieldWithPath("data.comments[].content").type(STRING).description("댓글 내용"),
+                    fieldWithPath("data.comments[].createdAt").type(STRING).description("댓글 작성날짜"),
+                    fieldWithPath("data.comments[].user").type(OBJECT).description("댓글 작성자"),
+                    fieldWithPath("data.comments[].user.id").type(NUMBER).description("작성자 아이디"),
+                    fieldWithPath("data.comments[].user.nickname").type(STRING).description("작성자 닉네임"),
+                    fieldWithPath("data.comments[].user.image").type(STRING).description("작성자 프로필 사진"),
+                    fieldWithPath("data.totalElements").type(NUMBER).description("전체 결과 수"),
+                    fieldWithPath("data.last").type(BOOLEAN).description("마지막 페이지 여부"),
+                    fieldWithPath("data.size").type(NUMBER).description("페이지당 요청 수"),
                     fieldWithPath("serverDateTime").type(STRING).description("서버 응답 시간")))
             );
     }
-
 }
