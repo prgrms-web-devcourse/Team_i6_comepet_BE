6ff0c83ac5cf914179761b4a77172f430e09aa5e
LeeJuOh <>
2021-12-07 20:27:19 +0900

    fix: gradle 수정

--- src/main/java/com/pet/common/config/WebMvcConfig.java
+++ src/main/java/com/pet/common/config/WebMvcConfig.java
@@ -17,13 +17,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
             .addPathPatterns("/api/v1/**");
     }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-            .addResourceHandler("/static/**")
-            .addResourceLocations("classpath:/static/")
-            .setCachePeriod(10);
-    }
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry
+//            .addResourceHandler("/static/**")
+//            .addResourceLocations("classpath:/static/")
+//            .setCachePeriod(10);
+//    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
