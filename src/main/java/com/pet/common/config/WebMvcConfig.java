4e56031849e132a8a64d0cbde7b632dcf8562cf3
LeeJuOh <>
2021-12-07 20:35:32 +0900

    fix: gradle 수정

--- src/main/java/com/pet/common/config/WebMvcConfig.java
+++ src/main/java/com/pet/common/config/WebMvcConfig.java
@@ -4,7 +4,6 @@ import com.pet.common.interceptor.JwtAuthenticationInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
