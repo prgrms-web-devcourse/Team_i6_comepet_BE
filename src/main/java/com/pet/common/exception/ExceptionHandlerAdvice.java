bf84f121757c755cca6098a95dfcb4f1e4f93295
yhh1056 <yhh1056@naver.com>
2021-12-09 19:00:15 +0900

    style: e 변수명 수정

--- src/main/java/com/pet/common/exception/ExceptionHandlerAdvice.java
+++ src/main/java/com/pet/common/exception/ExceptionHandlerAdvice.java
@@ -4,7 +4,6 @@ import com.pet.common.exception.httpexception.BadRequestException;
 import com.pet.common.exception.httpexception.NotFoundException;
 import com.pet.common.exception.httpexception.UnauthorizedException;
 import com.pet.common.response.ErrorResponse;
-import javax.naming.AuthenticationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,29 +16,29 @@ public class ExceptionHandlerAdvice {
 
     @ExceptionHandler(BadRequestException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleAuthenticationException(BadRequestException e) {
-        log.warn(e.getMessage());
-        return ErrorResponse.error(e.getMessage());
+    public ErrorResponse handleAuthenticationException(BadRequestException exception) {
+        log.warn(exception.getMessage());
+        return ErrorResponse.error(exception.getMessage());
     }
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleNotFoundException(NotFoundException e) {
-        log.warn(e.getMessage());
-        return ErrorResponse.error(e.getMessage());
+    public ErrorResponse handleNotFoundException(NotFoundException exception) {
+        log.warn(exception.getMessage());
+        return ErrorResponse.error(exception.getMessage());
     }
 
     @ExceptionHandler(UnauthorizedException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public ErrorResponse handleUnauthorizedException(UnauthorizedException e) {
-        log.warn(e.getMessage());
-        return ErrorResponse.error(e.getMessage());
+    public ErrorResponse handleUnauthorizedException(UnauthorizedException exception) {
+        log.warn(exception.getMessage());
+        return ErrorResponse.error(exception.getMessage());
     }
 
     @ExceptionHandler({Exception.class, RuntimeException.class})
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleAuthenticationException(Exception e) {
-        log.error(e.getMessage());
-        return ErrorResponse.error(e.getMessage());
+    public ErrorResponse handleAuthenticationException(Exception exception) {
+        log.error(exception.getMessage());
+        return ErrorResponse.error(exception.getMessage());
     }
 }
