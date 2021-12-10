2a353b90465e5e543d2cd476d14c75f24b477105
venzym <venzym@naver.com>
2021-12-11 08:37:54 +0900

    style: 안쓰이는 메소드 삭제

--- src/main/java/com/pet/domains/tag/repository/TagRepository.java
+++ src/main/java/com/pet/domains/tag/repository/TagRepository.java
@@ -3,16 +3,9 @@ package com.pet.domains.tag.repository;
 import com.pet.domains.tag.domain.Tag;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
     Optional<Tag> findTagByName(String name);
-
-    @Query("select t.count from Tag t where t.id = :tagId")
-    long findTagCountById(@Param("tagId") Long id);
-
-//    boolean existsTagByName(String name);
 
 }
