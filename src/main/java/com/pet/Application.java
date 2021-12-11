fa116deb46393844b20f485f0f6235e3510db4b0
LeeJuOh <>
2021-12-11 13:14:35 +0900

    chore: 스프링 애플리케이션 타임존 설정

--- src/main/java/com/pet/Application.java
+++ src/main/java/com/pet/Application.java
@@ -1,10 +1,15 @@
 package com.pet;
 
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Slf4j
 @EnableScheduling
 @SpringBootApplication
 @ConfigurationPropertiesScan("com.pet.common.property")
@@ -12,6 +17,13 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    public void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        log.info("server time: {}", LocalDateTime.now());
+
     }
 
 }
