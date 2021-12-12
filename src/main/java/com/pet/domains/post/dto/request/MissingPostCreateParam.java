58cdc4d6ade66ca400fbd65c2473bbb42482188b
venzym <venzym@naver.com>
2021-12-12 23:49:11 +0900

    feat: dto validation 추가

--- src/main/java/com/pet/domains/post/dto/request/MissingPostCreateParam.java
+++ src/main/java/com/pet/domains/post/dto/request/MissingPostCreateParam.java
@@ -2,37 +2,51 @@ package com.pet.domains.post.dto.request;
 
 import java.time.LocalDate;
 import java.util.List;
+import javax.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import reactor.util.annotation.Nullable;
 
 @Getter
 @NoArgsConstructor
 public class MissingPostCreateParam {
 
+    @NotBlank(message = "상태를 선택해주세요.")
     private String status;
 
+    @NotBlank(message = "날짜를 입력해주세요.")
     private LocalDate date;
 
+    @NotBlank(message = "시도를 선택해주세요.")
     private Long cityId;
 
+    @NotBlank(message = "시군구를 선택해주세요.")
     private Long townId;
 
     private String detailAddress;
 
+    @NotBlank(message = "전화번호를 입력해주세요.")
     private String telNumber;
 
+    @Nullable
     private Long animalId;
 
+    @Nullable
     private String animalKindName;
 
+    @Nullable
     private int age;
 
+    @NotBlank(message = "전화번호를 입력해주세요.")
     private String sex;
 
+    @Nullable
     private String chipNumber;
 
+    @Nullable
     private String content;
 
+    @Nullable
     private List<MissingPostCreateParam.Tag> tags;
 
     public MissingPostCreateParam(String status, LocalDate date, Long cityId, Long townId, String detailAddress,
