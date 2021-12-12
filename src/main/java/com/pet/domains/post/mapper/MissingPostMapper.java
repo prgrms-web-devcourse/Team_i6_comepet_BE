298589fd0a8f9616eca74d28fbf95c8d920e94f8
venzym <venzym@naver.com>
2021-12-12 23:48:12 +0900

    chore: account 추가 밑 동일 필드 삭제

--- src/main/java/com/pet/domains/post/mapper/MissingPostMapper.java
+++ src/main/java/com/pet/domains/post/mapper/MissingPostMapper.java
@@ -1,5 +1,6 @@
 package com.pet.domains.post.mapper;
 
+import com.pet.domains.account.domain.Account;
 import com.pet.domains.animal.domain.AnimalKind;
 import com.pet.domains.area.domain.Town;
 import com.pet.domains.post.domain.MissingPost;
@@ -13,23 +14,14 @@ public interface MissingPostMapper {
 
     @Mappings({
         @Mapping(target = "id", ignore = true),
-        @Mapping(target = "status", source = "param.status"),
-        @Mapping(target = "detailAddress", source = "param.detailAddress"),
-        @Mapping(target = "date", source = "param.date"),
-        @Mapping(target = "age", source = "param.age"),
         @Mapping(target = "sexType", source = "param.sex"),
-        @Mapping(target = "chipNumber", source = "param.chipNumber"),
-        @Mapping(target = "content", source = "param.content"),
-        @Mapping(target = "telNumber", source = "param.telNumber"),
         @Mapping(target = "viewCount", expression = "java(0)"),
         @Mapping(target = "bookmarkCount", expression = "java(0)"),
         @Mapping(target = "commentCount", expression = "java(0)"),
-        @Mapping(target = "thumbnail", source = "thumbnail"),
-        @Mapping(target = "account", expression = "java(null)"),
         @Mapping(target = "town", source = "townEntity"),
         @Mapping(target = "animalKind", source = "animalKindEntity")
     })
     MissingPost toEntity(MissingPostCreateParam param, Town townEntity,
-        AnimalKind animalKindEntity, String thumbnail);
+        AnimalKind animalKindEntity, String thumbnail, Account account);
 
 }
