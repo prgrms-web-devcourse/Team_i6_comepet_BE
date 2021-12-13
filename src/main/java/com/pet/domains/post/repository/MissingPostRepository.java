cd1f877cfe093149724b64707f3c4fb2ae380223
venzym <venzym@naver.com>
2021-12-14 03:34:50 +0900

    feat: 삭제에 필요한 쿼리메소드 생성

--- src/main/java/com/pet/domains/post/repository/MissingPostRepository.java
+++ src/main/java/com/pet/domains/post/repository/MissingPostRepository.java
@@ -1,8 +1,11 @@
 package com.pet.domains.post.repository;
 
+import com.pet.domains.account.domain.Account;
 import com.pet.domains.post.domain.MissingPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MissingPostRepository extends JpaRepository<MissingPost, Long> {
+
+    void deleteMissingPostByIdAndAccount(Long postId, Account account);
 
 }
