357ea5f075b98c8e0a0272c22d88b2dced1d7f8b
venzym <venzym@naver.com>
2021-12-14 18:56:06 +0900

    feat: entity null 체크 추가

--- src/main/java/com/pet/domains/tag/domain/Tag.java
+++ src/main/java/com/pet/domains/tag/domain/Tag.java
@@ -11,6 +11,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.ObjectUtils;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -31,6 +32,9 @@ public class Tag extends BaseEntity {
 
     @Builder
     public Tag(String name, long count) {
+        ObjectUtils.requireNonEmpty(name, "name must not be null");
+        ObjectUtils.requireNonEmpty(count, "count must not be null");
+
         this.name = name;
         this.count = count;
     }
