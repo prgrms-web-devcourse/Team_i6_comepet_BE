10fcafbf07e4e78087a6ef9bb9f037e12f7c0262
yhh1056 <yhh1056@naver.com>
2021-12-15 00:05:11 +0900

    fix: 쿼리문 수정

--- src/main/java/com/pet/domains/area/repository/InterestAreaRepository.java
+++ src/main/java/com/pet/domains/area/repository/InterestAreaRepository.java
@@ -2,14 +2,14 @@ package com.pet.domains.area.repository;
 
 import com.pet.domains.area.domain.InterestArea;
 import java.util.List;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface InterestAreaRepository extends JpaRepository<InterestArea, Long> {
 
-    @Query("select i from InterestArea i join fetch i.town t left join fetch t.city c")
-    List<InterestArea> findByAccountId(Long id);
+    @Query("select i from InterestArea i join fetch i.town t left join fetch t.city c where i.account.id = :accountId")
+    List<InterestArea> findByAccountId(@Param("accountId") Long accountId);
 
     void deleteAllByAccountId(Long id);
 
