1a79fab4f240ab61c2c32cbb23f720e7403338bb
yhh1056 <yhh1056@naver.com>
2021-12-15 21:01:45 +0900

    feat: 에러 응답 코드 안내 api

--- src/main/java/com/pet/common/exception/controller/ExceptionController.java
+++ src/main/java/com/pet/common/exception/controller/ExceptionController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/error-info")
 public class ExceptionController {
 
+    /**
+     * 개발 단계에서 프론트에게 동적으로 에러 메세지를 전달하기 위해 생성
+     */
     @GetMapping
     public List<ErrorResponse> getExceptionMessage() {
         return Arrays.stream(ExceptionMessage.values())
