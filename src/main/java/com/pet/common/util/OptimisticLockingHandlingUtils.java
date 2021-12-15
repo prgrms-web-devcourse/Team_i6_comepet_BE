7ad90451fb311aac913348233e7e5118359a15c4
LeeJuOh <>
2021-12-16 03:00:56 +0900

    chore: description로 수정

--- src/main/java/com/pet/common/util/OptimisticLockingHandlingUtils.java
+++ src/main/java/com/pet/common/util/OptimisticLockingHandlingUtils.java
@@ -10,13 +10,13 @@ public class OptimisticLockingHandlingUtils {
         throw new AssertionError("유틸 클래스입니다.");
     }
 
-    public static void handling(Runnable runnable, int handlingCount, String runnerName) {
+    public static void handling(Runnable runnable, int handlingCount, String description) {
         for (int i = 0; i < handlingCount; i++) {
             try {
                 runnable.run();
                 break;
             } catch (ObjectOptimisticLockingFailureException e) {
-                log.warn("#{}: locking failure occurred when try {}", handlingCount, runnerName);
+                log.warn("#{}: locking failure occurred when try {}", handlingCount, description);
             }
         }
     }
