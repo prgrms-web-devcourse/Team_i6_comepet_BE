f9f888647d2b4520ef559e24353cf12e85a501b7
yhh1056 <yhh1056@naver.com>
2021-12-17 02:32:34 +0900

    feat: 알림 리스트 조회 api

--- src/main/java/com/pet/domains/account/repository/NotificationRepository.java
+++ src/main/java/com/pet/domains/account/repository/NotificationRepository.java
@@ -3,12 +3,20 @@ package com.pet.domains.account.repository;
 import com.pet.domains.account.domain.Account;
 import com.pet.domains.account.domain.Notification;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
     void deleteByIdAndAccount(Long noticeId, Account account);
 
     Optional<Notification> findByIdAndAccount(Long noticeId, Account account);
+
+    @EntityGraph(attributePaths = {"account", "missingPost"}, type = EntityGraphType.LOAD)
+    Page<Notification> findAll(Pageable pageable);
 
 }
