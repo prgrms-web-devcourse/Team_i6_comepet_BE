5bba666242cc20bf3c75cccded48391855e0af57
LeeJuOh <>
2021-12-17 16:57:15 +0900

    chore: 디버깅용 삭제

--- src/main/java/com/pet/common/filter/CorsFilter.java
+++ src/main/java/com/pet/common/filter/CorsFilter.java
@@ -29,7 +29,6 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
 
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
