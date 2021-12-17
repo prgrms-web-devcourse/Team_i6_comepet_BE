adb826445715708501db1f2ba756e60e47cc14b3
LeeJuOh <>
2021-12-17 15:42:33 +0900

    feat: allow header all

--- src/main/java/com/pet/common/filter/CorsFilter.java
+++ src/main/java/com/pet/common/filter/CorsFilter.java
@@ -32,8 +32,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        response.setHeader("Access-Control-Allow-Headers", "*");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
