409b49dabbd011591489e3f58f784a8ce074e3ac
LeeJuOh <>
2021-12-20 04:21:46 +0900

    fix: delete enable redis repo

--- src/main/java/com/pet/common/config/RedisConfig.java
+++ src/main/java/com/pet/common/config/RedisConfig.java
@@ -9,9 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
-@EnableRedisRepositories
 @RequiredArgsConstructor
 @Configuration
 public class RedisConfig {
