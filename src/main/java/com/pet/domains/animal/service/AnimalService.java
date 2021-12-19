03e96b014083968005952e50cf2d91c55d89a1d6
LeeJuOh <>
2021-12-20 06:34:00 +0900

    feat: test

--- src/main/java/com/pet/domains/animal/service/AnimalService.java
+++ src/main/java/com/pet/domains/animal/service/AnimalService.java
@@ -22,7 +22,7 @@ public class AnimalService {
 
     @Cacheable(
         cacheNames = "animals",
-        key = "#all"
+        key = "#all",
         unless = "#result == null || #result.getAnimals().isEmpty()"
     )
     public AnimalReadResults getAnimals() {
