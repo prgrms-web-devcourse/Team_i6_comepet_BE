02c396cbf85632e93fb69df62ad56749a314f912
LeeJuOh <>
2021-12-20 17:57:44 +0900

    chore: 로그파일 수정

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -3,14 +3,14 @@ BUILD_JAR=$(ls /home/ubuntu/app/compet-0.0.1.jar)
 DEPLOY_PATH=/home/ubuntu/app/
 JAR_NAME=$(basename $BUILD_JAR)
 echo "> dev deploy"
-echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/app/log/deploy.log
+echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/app/log/deploy_dev.log
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/app/deploy.log
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/app/deploy_dev.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/app/deploy.log
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/app/deploy_dev.log
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
@@ -19,10 +19,10 @@ fi
 
 source ~/.env
 ENVS=$(printenv)
-echo "> 환경변수 :$ENVS" >> /home/ubuntu/app/deploy.log
+echo "> 환경변수 :$ENVS" >> /home/ubuntu/app/deploy_dev.log
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 이름: $DEPLOY_JAR"    >> /home/ubuntu/app/deploy.log
+echo "> DEPLOY_JAR 이름: $DEPLOY_JAR"    >> /home/ubuntu/app/deploy_dev.log
 chmod 755 $DEPLOY_JAR
-echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/app/deploy.log
+echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/app/deploy_dev.log
 nohup java -jar -Duser.timezone=Asia/Seoul -Dspring.profiles.active=dev $DEPLOY_JAR >> /home/ubuntu/app/deploy_dev.log 2>/home/ubuntu/app/deploy_dev_err.log &
