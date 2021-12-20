723fcf2fcda01ee760dd380cc50c0724a9d2e93c
yhh1056 <yhh1056@naver.com>
2021-12-21 02:12:06 +0900

    feat: token값 파라미터로 던짐

--- src/main/java/com/pet/common/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/pet/common/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -34,9 +34,9 @@ public class OAuth2AuthenticationSuccessHandler extends SavedRequestAwareAuthent
 
             String redirectUri = determineTargetUrl(request, response, authentication);
             String targetUrl = UriComponentsBuilder
-                .fromUriString(redirectUri).queryParam("token", token)
+                .fromUriString("https://comepet.netlify.app/oauth/redirect?").queryParam("token", token)
                 .build().toUriString();
-            getRedirectStrategy().sendRedirect(request, response, "https://comepet.netlify.app/oauth/redirect");
+            getRedirectStrategy().sendRedirect(request, response, targetUrl);
             // String loginSuccessJson =  "{\"username\": \"" + account.getId() + "\", \"token\":\""
             //     + token + "\"}";
             // setResponse(response, loginSuccessJson);
