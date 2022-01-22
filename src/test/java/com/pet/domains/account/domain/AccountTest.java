ad5ef2674cc50ab0deb259fff00f5bb533b70970
yhh1056 <yhh1056@naver.com>
2022-01-22 14:30:43 +0900

    feat: 회원 객체 생성시 이메일 검증

--- src/test/java/com/pet/domains/account/domain/AccountTest.java
+++ src/test/java/com/pet/domains/account/domain/AccountTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @DisplayName("회원 테스트")
 class AccountTest {
@@ -21,6 +22,27 @@ class AccountTest {
             .build();
 
         assertTrue(account.isMatchPassword(passwordEncoder, "123123a!"));
+    }
+
+    @Test
+    @DisplayName("올바른 이메일 형식으로 회원 객체 생성 테스트")
+    void validateEmail() {
+        Account.builder()
+            .email("tester@email.com")
+            .nickname("tester")
+            .group(new Group("group"))
+            .build();
+    }
+
+    @Test
+    @DisplayName("올바르지 않은 이메일 형식으로 회원 객체 생성 테스트")
+    void invalidEmailTest() {
+        assertThrows(IllegalArgumentException.class,
+            () -> Account.builder()
+                .email("email.com")
+                .nickname("tester")
+                .group(new Group("group"))
+                .build());
     }
 
 }
