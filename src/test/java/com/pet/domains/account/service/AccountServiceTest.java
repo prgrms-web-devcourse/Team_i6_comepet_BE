04f87f5a852dece2ec28aedb2d767dfa65f1aacb
yhh1056 <yhh1056@naver.com>
2022-01-22 12:40:54 +0900

    feat: 관심 지역 등록 실패 테스트

--- src/test/java/com/pet/domains/account/service/AccountServiceTest.java
+++ src/test/java/com/pet/domains/account/service/AccountServiceTest.java
@@ -1,5 +1,6 @@
 package com.pet.domains.account.service;
 
+import com.pet.common.exception.httpexception.NotFoundException;
 import com.pet.domains.account.domain.Account;
 import com.pet.domains.account.dto.request.AccountAreaUpdateParam;
 import com.pet.domains.account.repository.AccountRepository;
@@ -9,7 +10,7 @@ import com.pet.domains.area.domain.Town;
 import com.pet.domains.area.mapper.InterestAreaMapper;
 import com.pet.domains.area.repository.InterestAreaRepository;
 import com.pet.domains.area.repository.TownRepository;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
@@ -42,7 +43,7 @@ class AccountServiceTest {
 
     @Test
     @DisplayName("지역 2개 변경 성공 테스트")
-    void set2AreaTests() {
+    void set2AreaTest() {
         Account account = mock(Account.class);
         AccountAreaUpdateParam.Area area1 = new AccountAreaUpdateParam.Area(1L, true);
         AccountAreaUpdateParam.Area area2 = new AccountAreaUpdateParam.Area(2L, false);
@@ -64,7 +65,7 @@ class AccountServiceTest {
 
     @Test
     @DisplayName("지역 1개 변경 성공 테스트")
-    void set1AreaTests() {
+    void set1AreaTest() {
         Account account = mock(Account.class);
         AccountAreaUpdateParam.Area area1 = new AccountAreaUpdateParam.Area(1L, true);
         AccountAreaUpdateParam param = new AccountAreaUpdateParam(List.of(area1), true);
@@ -80,5 +81,48 @@ class AccountServiceTest {
 
         verify(interestAreaRepository, times(1)).deleteAllByAccountId(account.getId());
         verify(interestAreaRepository, times(1)).save(any());
+    }
+
+    @Test
+    @DisplayName("디폴트 지역이 2개로 요청된 경우 실패테스트")
+    void set2DefaultAreaFailTest() {
+        Account account = mock(Account.class);
+        AccountAreaUpdateParam.Area area1 = new AccountAreaUpdateParam.Area(1L, true);
+        AccountAreaUpdateParam.Area area2 = new AccountAreaUpdateParam.Area(2L, true);
+        AccountAreaUpdateParam param = new AccountAreaUpdateParam(List.of(area1, area2), true);
+
+        assertThrows(NotFoundException.class, () -> accountService.updateArea(account, param));
+    }
+
+    @Test
+    @DisplayName("디폴트 지역 없이 요청된 경우 실패테스트")
+    void setNoDefaultAreaFailTest() {
+        Account account = mock(Account.class);
+        AccountAreaUpdateParam.Area area1 = new AccountAreaUpdateParam.Area(1L, false);
+        AccountAreaUpdateParam.Area area2 = new AccountAreaUpdateParam.Area(2L, false);
+        AccountAreaUpdateParam param = new AccountAreaUpdateParam(List.of(area1, area2), true);
+
+        assertThrows(NotFoundException.class, () -> accountService.updateArea(account, param));
+    }
+
+    @Test
+    @DisplayName("관심 지역이 없는 경우 실패 테스트")
+    void setNoAreaFailTest() {
+        Account account = mock(Account.class);
+        AccountAreaUpdateParam param = new AccountAreaUpdateParam(Collections.emptyList(), true);
+
+        assertThrows(NotFoundException.class, () -> accountService.updateArea(account, param));
+    }
+
+    @Test
+    @DisplayName("관심 지역이 2개가 넘는 경우 실패 테스트")
+    void setOverSizeAreaFailTest() {
+        Account account = mock(Account.class);
+        AccountAreaUpdateParam.Area area1 = new AccountAreaUpdateParam.Area(1L, true);
+        AccountAreaUpdateParam.Area area2 = new AccountAreaUpdateParam.Area(2L, true);
+        AccountAreaUpdateParam.Area area3 = new AccountAreaUpdateParam.Area(3L, true);
+        AccountAreaUpdateParam param = new AccountAreaUpdateParam(List.of(area1, area2, area3), true);
+
+        assertThrows(NotFoundException.class, () -> accountService.updateArea(account, param));
     }
 }
