7149697847d803fd16ba15d4622b3f25dc927dd3
yhh1056 <yhh1056@naver.com>
2022-02-12 15:51:49 +0900

    fix: 리프레시 토큰 디비에서 관리하도록 수정
    레디스 연결에 실패함

--- src/main/java/com/pet/domains/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/pet/domains/auth/repository/RefreshTokenRepository.java
@@ -1,8 +1,12 @@
 package com.pet.domains.auth.repository;
 
 import com.pet.domains.auth.oauth2.RefreshToken;
+import java.util.Optional;
+import java.util.stream.DoubleStream;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
+    Optional<RefreshToken> findByToken(String refreshToken);
 }
