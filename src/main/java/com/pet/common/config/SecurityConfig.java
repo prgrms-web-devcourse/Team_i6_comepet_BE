4dd9c4d98e9482792f32c5563af09f44097d7978
yhh1056 <yhh1056@naver.com>
2022-02-12 22:56:11 +0900

    feat: allowedOrigin 수정

--- src/main/java/com/pet/common/config/SecurityConfig.java
+++ src/main/java/com/pet/common/config/SecurityConfig.java
@@ -168,8 +168,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.addAllowedOrigin("*");
+        configuration.addAllowedOrigin("http://localhost:3000");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
